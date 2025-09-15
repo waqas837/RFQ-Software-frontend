6298054ff69f95a1f96ff1435f7ea80894a89d5c
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-09-15 21:44:22 +0500

    laravel deployed api url changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://127.0.0.1:8000/api'
+const API_BASE_URL = 'http://62.171.176.47:8081/api'
 
 // Helper functions
 const getAuthToken = () => localStorage.getItem('authToken')
